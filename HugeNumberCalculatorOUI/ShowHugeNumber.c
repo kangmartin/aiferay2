81165fd681f71448f2c8e2f62bd6dfc8605e7e96
Martin Kang <88689251+kangmartin@users.noreply.github.com>
2022-05-26 14:55:09 +0200

    Create ShowHugeNumber.c

---
+++ HugeNumberCalculatorOUI/ShowHugeNumber.c
@@ -0,0 +1,34 @@
+#include "ShowHugeNumber.h"
+#include <stdio.h>
+
+#define SIZE_OF_EXPONENT_MARKUP 2
+#define EXPONENT_MARKUP " E"
+
+static char SignToChar (const Sign sign) {
+    return 0;
+}
+
+static char* HugeUnsignedIntToString (const HugeUnsignedInt* hugeUnsignedInt) {
+    return NULL;
+}
+
+static char* HugeIntToString (const HugeInt* hugeInt) {
+    return NULL;
+}
+
+static char* HugeFloatToString (const HugeFloat* hugeFloat) {
+    return NULL;
+}
+
+
+void printHugeInt (const HugeInt* hugeInt) {
+}
+
+void printHugeFloat (const HugeFloat* hugeFloat) {
+}
+
+void printHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
+    char* hugeUnsignedIntString = HugeUnsignedIntToString (hugeUnsignedInt);
+    printf ("%s\n", hugeUnsignedIntString);
+    free (hugeUnsignedIntString);
+}
